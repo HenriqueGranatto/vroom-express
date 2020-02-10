0962e3a86492f1ebf8c80ed5080cfc5138d9900c
henriquegranatto <henrique.ramires.granatto@gmail.com>
2020-02-10 15:30:10 -0300

    .

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@ const http = require('http')
 const {app} = require('../app')
 require('dotenv').config()
 
-const port = process.env.PORT
+const port = process.env.API_PORT
 
 const server = http.createServer(app)
 
