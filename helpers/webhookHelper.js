77411eec00a0f49c56e331333d8bad8d7b775fb4
Henrique Granatto <henrique.ramires.granatto@gmail.com>
2020-07-28 18:41:30 -0300

    Update webhookHelper.js

--- helpers/webhookHelper.js
+++ helpers/webhookHelper.js
@@ -63,7 +63,7 @@ exports.sendToObserver = async (settingsToRequest) =>
             else
             {
                 axios(request).then(() => {
-                    helper.insertInDB("notificationLog", {process: process.env.REQUEST_START, event: "NOTIFICATION_SENDED", token: settingsToRequest.token, date: (new Date).toLocaleString(), data: request})
+                    helper.insertInDB("notificationLog", JSON.stringfy({process: process.env.REQUEST_START, event: "NOTIFICATION_SENDED", token: settingsToRequest.token, date: (new Date).toLocaleString(), data: request}))
                 })
             }
         })
